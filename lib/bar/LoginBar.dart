e6737b0f261ebdadcba700208cb6491fa9d5567e
Yoo <hiyoojeong@gmail.com>
2023-06-10 01:58:52 +0900

    Feat: MainBar 구현

--- lib/bar/LoginBar.dart
+++ lib/bar/LoginBar.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HeaderLogin extends StatefulWidget implements PreferredSizeWidget {
-  const HeaderLogin({Key? key, required this.title}) : super(key: key);
+class LoginBar extends StatefulWidget implements PreferredSizeWidget {
+  const LoginBar({Key? key, required this.title}) : super(key: key);
 
   final String title;
   final double header_height = 60; // 헤더 높이
 
   @override
-  State<HeaderLogin> createState() => _HeaderLoginState();
+  State<LoginBar> createState() => _LoginBarState();
 
   @override
   Size get preferredSize => Size.fromHeight(header_height);
 }
 
-class _HeaderLoginState extends State<HeaderLogin> {
+class _LoginBarState extends State<LoginBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
---
+++ lib/bar/MainBar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainBar extends StatefulWidget implements PreferredSizeWidget {
+  const MainBar({Key? key}) : super(key: key);
+
+  final double header_height = 60; // 헤더 높이
+
+  @override
+  State<MainBar> createState() => _MainBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(header_height);
+}
+
+class _MainBarState extends State<MainBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text("덕지순례"),
+      centerTitle: true,
+      backgroundColor: Colors.black,
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.search),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.settings),
+        )
+      ],
+    );
+  }
+}
